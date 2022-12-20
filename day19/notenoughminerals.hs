4c3dcd109221558a725fcc326c921041eaf12b3d
Florent Bouchez Tichadou <florent.bouchez-tichadou@imag.fr>
2022-12-20 06:02:43 +0100

    working for part 2 although i though it would be too long

--- day19/notenoughminerals.hs
+++ day19/notenoughminerals.hs
@@ -4,6 +4,8 @@ import Data.List
 import Data.Maybe
 import Data.Function
 import Data.Array
+import Data.Tuple.Extra
+
 import Debug.Trace
 import qualified Data.Text as T
 import qualified Data.Sequence as S
@@ -11,8 +13,10 @@ import Text.ParserCombinators.ReadP
 
 
 -- inittime = 10 -- for testing purposes
-inittime = 24 -- for testing purposes
--- inittime = 24
+-- inittime = 19 -- for testing purposes
+-- inittime = 21 -- for testing purposes
+-- inittime = 24 -- part 1
+inittime = 32 -- part 2
 
 parseBluePrints = do
     -- bp <- parseBluePrint
@@ -71,8 +75,24 @@ parseBluePrint = do
 analyseBP id orecost claycost (obsidcostOre, obsidcostClay) (geodecostOre, geodecosteObs) =
     BluePrint id orecost claycost (obsidcostOre, obsidcostClay) (geodecostOre, geodecosteObs) maxorerate maxclayrate maxobsrate
     where maxorerate = maximum [orecost, claycost, obsidcostOre, geodecostOre]
+          -- maxclayrate = (obsidcostClay + 2) `div` 2      -- heuristics to speedup only for part 1
           maxclayrate = obsidcostClay
           maxobsrate = geodecosteObs
+
+
+
+data SimuState = SimuState {
+        bluep :: BluePrint,
+        best :: Array Int [Status] -- keep the best status found at each point
+} deriving (Show)
+
+
+
+-- add new state and remove all states that are lower
+updateSimuState sim@(SimuState bp best) st@(Status time r m) =
+    -- trace ("Updating list from\n" ++ show (best!time) ++ "\n" ++ show stupdate) $
+    sim { best = best // [(time, stupdate )] }
+    where stupdate  = st:(filter (not . flip lowerStatus st) $ best!time)
 
 
 main = do
@@ -80,29 +100,56 @@ main = do
     let blueprintsraw = readP_to_S parseBluePrints contents
         blueprints = fst . head $ blueprintsraw
 
-    print blueprintsraw
+    -- print blueprintsraw
     print blueprints
 
-    let bp = blueprints!!1
+    -- let bp = blueprints!!0
+    let initsimstate bp = SimuState {
+          bluep = bp,
+          best = listArray (0, inittime) $ repeat []
+        }
 
 
-    -- let testBuy = [Rclay, Rclay, Rclay, Robs, Rclay, Robs, Rgeo, Rgeo]
-        -- sol = simulate (head blueprints) initrobstate initminstate inittime testBuy
+    -- let bp = blueprints!!0
+    -- let testpath = [Rore, Rore, Rclay, Rclay, Rclay, Rclay, Rclay, Rclay, Robs, Robs, Robs, Rgeo, Rgeo]
+        -- sol = simulate bp initrobstate initminstate inittime testpath
     -- print sol
 
-    -- let (numgeodes, path) = bruteForceBP [] (head blueprints) initrobstate initminstate inittime
-    let (numgeodes, path) = bruteForceBP [] bp initrobstate initminstate inittime
 
+    let dobrute bp = bruteForceBP (initsimstate bp) [] bp initrobstate initminstate inittime
+    let allbest = 
+          if inittime == 32 then map dobrute $ take 3 blueprints
+                            else map dobrute blueprints
+
+    mapM_ showthebest allbest
+
+    let result = 
+          if inittime == 32 then computeResult32 allbest
+                            else computeResult allbest
+    putStrLn $ "Résultat : " ++ show result
+
+    -- let (numgeodes, path, sims) = bruteForceBP initsimstate [] bp initrobstate initminstate inittime
     -- putStrLn $ "Nombre de géodes ouvertes: " ++ show numgeodes
     -- putStrLn $ "Simulating path: " ++ show (reverse path)
 
-    let testpath = [Rnone,Rnone,Rore,Rnone,Rore,Rclay,Rclay,Rclay,Rclay,Rclay,Rclay,Robs,Robs,Robs,Robs,Rnone,Robs,Rgeo,Robs,Rgeo,Rnone,Rgeo,Rnone,Rore]
+    -- let testpath = [Rnone,Rnone,Rore,Rnone,Rore,Rclay,Rclay,Rclay,Rclay,Rclay,Rclay,Robs,Robs,Robs,Robs,Rnone,Robs,Rgeo,Robs,Rgeo,Rnone,Rgeo,Rnone,Rore]
+    -- let sol = simulate bp initrobstate initminstate inittime $ testpath
 
-    let sol = simulate bp initrobstate initminstate inittime $ testpath
-    -- let sol = simulate bp initrobstate initminstate inittime $ reverse 
-    -- path
-    print sol
+    -- let sol = simulate bp initrobstate initminstate inittime $ reverse path
+    -- print sol
 
+computeResult allbest = foldl addBPres 0 allbest
+  where addBPres accu (bgeo, _, sims) = accu + (ident . bluep $ sims) * bgeo
+
+showthebest (bgeo, bpath, sims) = do
+    putStrLn $ "Blueprint number: " ++ show (bluep sims)
+    putStrLn $ "Nombre de géodes ouvertes: " ++ show bgeo
+    putStrLn $ "Best path: " ++ show (reverse bpath)
+    mapM_ (\(t, s) -> putStr $ show t ++ "-" ++ show (length s) ++ " ") $ assocs (best sims)
+    putStrLn ""
+
+computeResult32 allbest = foldl multPres 1 allbest
+  where  multPres accu (bgeo, _, _) = accu * bgeo
 
 
 data Robot = Rnone | Rore | Rclay | Robs | Rgeo deriving(Eq, Show)
@@ -137,6 +184,21 @@ instance Show Status where
       " Clay: " ++ show (rclay r) ++ "/" ++ show (clay m) ++
       " Obsidan: " ++ show (robs r) ++ "/" ++ show (obsidian m) ++
       " Geodes: " ++ show (rgeo r) ++ "/" ++ show (geode m)
+
+
+-- return true if left status is inferior in all points to right status
+lowerStatus (Status time r m) (Status time' r' m')
+  | time /= time' = False
+  | otherwise =
+      rore r <= rore r' &&
+      rclay r <= rclay r' &&
+      robs r <= robs r' &&
+      rgeo r <= rgeo r' &&
+      ore m <= ore m' &&
+      clay m <= clay m' &&
+      obsidian m <= obsidian m' &&
+      geode m <= geode m'
+
 
 
 nextMinState robots mst =
@@ -212,33 +274,54 @@ simulateTurn bp robots minst rob = (robots', minst'')
 
 
 
-buildchoices = reverse [Rnone, Rore, Rclay, Robs, Rgeo]
-realbuildchoices = reverse [Rore, Rclay, Robs, Rgeo]
+-- buildchoices = reverse [Rnone, Rore, Rclay, Robs, Rgeo]
+-- realbuildchoices = reverse [Rore, Rclay, Robs, Rgeo]
 
 
-bruteForceBP path bp robots minst 0 = (geode minst, path)
-bruteForceBP path bp robots minst timeleft =
-    (if length path < 10 then trace ("Trying path " ++ show (reverse path))
-                        else id)
-    takeBest $ tryAllPossibleChoices
-  where tryAllPossibleChoices = map tryChoice choices
+bruteForceBP sims path bp robots minst 0 = (geode minst, path, sims)
+bruteForceBP sims path bp robots minst timeleft =
+    let currentbests = (best sims)!timeleft
+        currstatus = Status timeleft robots minst
 
-        initchoices = if uselessToWaitMore bp robots minst
-                        then trace ("No use to wait more") $
-                             realbuildchoices
-                        else buildchoices
+        dotrace = length path < inittime - 16
+        dosimupdate = length path < inittime - 9
+    in
 
-        choicesa = filter (canBuildRobot bp minst) initchoices
+    (if dotrace
+      then trace ("Trying path " ++ show (reverse path) ++
+                  "\nSimState " ++ show currentbests ++
+                  "\nCurrentState " ++ show currstatus)
+      else id) $
+    -- start by checking if we are worse that a previously seen state at 
+    -- that point
 
-        -- choices = filter tooMuchMins choisesa
-        choices = choicesa
+    if any (lowerStatus currstatus) currentbests
+      then (if dotrace 
+             then trace ("Lower status, not continuing " ++ show (reverse path))
+             else id)
+           $
+           (0, [], sims)
+      else let
+        sims' = if dosimupdate then updateSimuState sims currstatus
+                               else sims
+        initchoices = makeChoiceList bp robots minst
+        choices = filter (canBuildRobot bp minst) initchoices
 
-        tryChoice r =
-          let (robots', minst') = simulateTurn bp robots minst r
-          in
-          bruteForceBP (r:path) bp robots' minst' (timeleft - 1)
+        tryChoice cursim r =
+              let (robots', minst') = simulateTurn bp robots minst r
+              in
+              bruteForceBP cursim (r:path) bp robots' minst' (timeleft - 1)
 
-        takeBest = maximumBy (compare `on` fst)
+        -- takeBest = maximumBy (compare `on` fst3)
+
+
+        accuTryChoice (bgeo, bpath, bsims) r =
+          let (geo, path, sims'') = tryChoice bsims r
+          in if (geo > bgeo) then (geo, path, sims'')
+                             else (bgeo, bpath, sims'')
+
+        result = foldl accuTryChoice (0, [], sims') choices
+        in result
 
 
 -- inutile d'attendre sans construire de robot si
@@ -267,4 +350,16 @@ uselessToWaitMore bp robots minst =
 -- if we produce more mins per turn than we can consume...
 --
 --
--- tooMuchMins = 
+makeChoiceList bp robots minst = Rgeo : (addOre . addClay . addObs) initl
+   where initl = if uselessToWaitMore bp robots minst 
+                   then --trace ("No use to wait more") $
+                        []
+                   else [Rnone]
+
+         addObs l = if robs robots < maxobsrate bp then Robs : l else l
+         addClay l = if rclay robots < maxclayrate bp then Rclay : l else l
+         addOre l = if rore robots < maxorerate bp then Rore : l else l
+
+
+
+
